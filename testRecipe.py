eac86af55c9f0c94e43982185eaa546b118d38b2
Fatehjot <gogiaf@oregonstate.edu>
2025-02-25 21:39:29 +0000

    Feb 25, 2025, 1:39 PM

--- testRecipe.py
+++ testRecipe.py
@@ -10,7 +10,7 @@ def getRecipe():
     return recipe
 
 def main():
-    recipeParameters = ['run', 'Chinese', 'Main Course', 'Savory']
+    recipeParameters = ['run', 'Polish', 'Main Course', 'Savory']
     writeInitial(recipeParameters)
     time.sleep(8)
     recipe = getRecipe()
